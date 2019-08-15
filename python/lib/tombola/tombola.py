9423b6529bae251792682a654f4766b3d0df4d2e
quentin <quentinrichard97@gmail.com>
2019-08-15 02:07:24 +0200

    add model tombola

--- python/lib/tombola/tombola.py
+++ python/lib/tombola/tombola.py
@@ -1,38 +1,122 @@
+"""
+table tombola:
+ - id (pk)
+ - id_transaction varchar
+ - id_user varchar
+ - status ['W' > progress, 'A' > abort, 'V' > valid]
+ - ticket1
+ - ticket5
+ - ticket10
+"""
+
 import requests
 from db import dbskiut_con
 from config.urls import _SKIUTC_SERVICE, _CAS_URL
+import json
 
+from weezevent.weezevent_api import WeezeventAPI
 
 class Tombola():
     """
     Tombola methods
     """
-    def __init__(self, user):
-        self.user = user
+    # def __init__(self, user):
+    #     self.user = user
 
-    def insert_transaction(self, transaction):
-        """
-        Add the transaction to the database
-        with user_id and weez_trans number
-        """
-
-        con = dbskiut_con
-        login = user.get_login()
-
-        with con:
-            cur = con.cursor()
-            sql = "INSERT INTO `tombola_2020` (`user`, `weez_trans`) VALUES (%s, %s)"
-            cur.execute(sql, login, transaction)
-
-    def validate_transaction(self, ticket, ticket5, ticket10):
+    def buy_tombola(self, user, ticket1, ticket5, ticket10):
         """
         Do the transaction on weezevent side
         in order to validate the buy
         """
-    def get_transactions(self):
-        """
-        Return all transactions done by the user
-        and what ticket he bought
-        """
+        api = WeezeventAPI()
 
-    
+        tickets = [
+            ['15122', ticket1],
+            ['15123', ticket10],
+            ['15121', ticket5]
+        ]
+
+        response = api.create_transaction(tickets, user.get_email(), 'http://assos.utc.fr/baignoirutc')
+
+        con = dbskiut_con()
+        con.begin()
+        with con:
+            try:
+                cur = con.cursor()
+                sql = "INSERT INTO `tombola_2020` (`id_transaction`, `login_user`, `status`, `ticket1`, `ticket5`, `ticket10`) VALUES (%s, %s, %s, %s, %s, %s)"
+                cur.execute(sql, (response['tra_id'], user.get_login(), 'W', int(ticket1), int(ticket5), int(ticket10)))
+                con.commit()
+            except Exception as e:
+                raise e
+            finally:
+                cur.close()
+
+        return response
+
+    #Call the transaction from weezevent and update its status if it has changed
+    def update_transaction_status(self, transaction):
+        api = WeezeventAPI()
+
+        response = api.get_transaction_info(transaction)
+
+        if response['status'] == 'V':
+            con = dbskiut_con()
+            con.begin()
+            try:
+                cur = con.cursor()
+                sql = "UPDATE `tombola_2020` SET `status`=%s WHERE `id_transaction`=%s"
+                cur.execute(sql, (response['status'], transaction))
+                con.commit()
+            except Exception as e:
+                raise e
+            finally:
+                cur.close()
+
+    #Get user stats for tombola
+    def get_user_stats(self, user):
+        response = {}
+
+        con = dbskiut_con()
+        con.begin()
+        try:
+            print('begin try')
+            cur = con.cursor()
+            sql = "SELECT SUM(`ticket1`) as `ticket1`, SUM(`ticket5`) as `ticket5`, SUM(`ticket10`) as `ticket10` from `tombola_2020` where `login_user`=%s and `status`=%s"
+            print('sql ok')
+            cur.execute(sql, (user.get_login(), 'V'))
+            con.commit()
+            print('execute ok')
+            response = cur.fetchone()
+            response = {
+                'ticket1': int(response['ticket1']),
+                'ticket5': int(response['ticket5']),
+                'ticket10': int(response['ticket10']),
+            }
+
+            print(response)
+        except Exception as e:
+            raise e
+        finally:
+            cur.close()
+
+        return json.dumps(response)
+
+    def validate_tombola(self, user):
+        con = dbskiut_con()
+        response = None
+        con.begin()
+        try:
+             cur = con.cursor()
+             sql = "SELECT `id_transaction` from `tombola_2020` where `login_user`=%s ORDER BY `id` DESC"
+             cur.execute(sql, user.get_login())
+             con.commit()
+             transaction = cur.fetchone()
+        except Exception as e:
+            raise e
+        finally:
+            cur.close()
+
+        #Update the transaction status in the bdd
+        self.update_transaction_status(transaction['id_transaction'])
+
+        return self.get_user_stats(user)
