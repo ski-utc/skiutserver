322608caba2d4cd39c24134ba6bd1b4d09edcc95
hugofloter <hugo.paigneau@etu.utc.fr>
2019-09-22 14:03:40 +0200

    meta returns user info if is shotgun

--- python/lib/webapis/meta.py
+++ python/lib/webapis/meta.py
@@ -24,6 +24,9 @@ def get_meta():
     if user_auth_inst is None:
         return meta
     else:
+        user = User.build_user_from_login(user_auth_inst["login"])
+        info_user = user.get_user_info()
+        meta["user"]["info"] = info_user
         meta["user"]["login"] = user_auth_inst["login"]
         meta["user"]["auth"] = True
         return meta
