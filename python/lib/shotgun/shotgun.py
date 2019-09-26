0690e54435c195d60b28253b44b42ae8632425be
hugofloter <hugo.paigneau@etu.utc.fr>
2019-09-26 11:25:11 +0200

    shotgun check

--- python/lib/shotgun/shotgun.py
+++ python/lib/shotgun/shotgun.py
@@ -2,6 +2,7 @@ from user.user import User
 from db import dbskiut_con
 from globals import send_skiutc_mail
 import pymysql
+from datetime import datetime, timedelta
 
 class Shotgun:
     """
@@ -26,39 +27,65 @@ class Shotgun:
         self.shotgun = False
         self._add_to_shotgun()
 
+    @staticmethod
+    def _organize_table():
+        con = dbskiut_con()
+        con.begin()
+        with con:
+            cur = con.cursor()
+            sql = "SELECT * FROM `shotgun-etu_2020` WHERE idshotgun < 301"
+            try:
+                cur.execute(sql)
+            except pymysql.err.InternalError as error:
+                code, message = error.args
+                print (">>>>>>>>>>>>>", code, message)
+            except pymysql.err.IntegrityError as error:
+                code, message = error.args
+                print (">>>>>>>>>>>>>", code, message)
+            shotgun_list = cur.fetchall()
+            for shotgun in shotgun_list:
+                user = User.build_user_from_login(shotgun["login"])
+                if not user.is_valid() or not user.is_cotisant():
+                    sql = "DELETE FROM `shotgun-etu_2020` WHERE login=%s"
+                    try:
+                        cur.execute(sql,shotgun["login"])
+                    except pymysql.err.InternalError as error:
+                        code, message = error.args
+                        print(">>>>>>>>>>>>>", code, message)
+                    except pymysql.err.IntegrityError as error:
+                        code, message = error.args
+                        print(">>>>>>>>>>>>>", code, message)
+                    cur.commit()
+            cur.close()
+
     def _add_to_shotgun(self):
         """
         Function to add row in shotgun used for the shotgun
-        we check for cotisation and login trusted
-        login cannot be added twice
+        Need to check after for cotisant and valid !
         :return: True if added, false else
         """
-        user = User.build_user_from_login(self.login)
-        if user.is_valid() and user.is_cotisant():
-            con = dbskiut_con()
-            con.begin()
-            with con:
-                cur = con.cursor()
-                sql = "INSERT INTO `shotgun-etu_2020` (login) VALUES (%s)"
-                try:
-                    cur.execute(sql, self.login)
-                    con.commit()
-                except pymysql.InternalError as error:
-                    code, message = error.args
-                    print (">>>>>>>>>>>>>", code, message)
+        con = dbskiut_con()
+        con.begin()
+        with con:
+            cur = con.cursor()
+            sql = "INSERT INTO `shotgun-etu_2020` (login) VALUES (%s)"
+            try:
+                cur.execute(sql, self.login)
+                con.commit()
+            except pymysql.InternalError as error:
+                code, message = error.args
+                print (">>>>>>>>>>>>>", code, message)
+                self.shotgun = False
+            except pymysql.err.IntegrityError as error:
+                code, message = error.args
+                print (">>>>>>>>>>>>>", code, message)
+                self.shotgun = False
+            finally:
+                if cur.lastrowid:
+                    self.shotgun = True
+                else:
                     self.shotgun = False
-                except pymysql.err.IntegrityError as error:
-                    code, message = error.args
-                    print (">>>>>>>>>>>>>", code, message)
-                    self.shotgun = False
-                finally:
-                    if cur.lastrowid:
-                        self.shotgun = True
-                    else:
-                        self.shotgun = False
-                    cur.close()
-        else:
-            self.shotgun = False
+                cur.close()
 
     def shotgun_valid(self):
         return self.shotgun
@@ -86,6 +113,14 @@ class Shotgun:
             shotgun = cur.fetchall()
             #array of all shotgun in object
             return shotgun
+
+    @staticmethod
+    def check_time():
+        valid = False
+        validity_date = datetime(2019, 10, 14, 4, 0, 0, 0)
+        if datetime.now() - validity_date > timedelta(0):
+            valid = True
+        return valid
 
     @staticmethod
     def send_result_mail():
