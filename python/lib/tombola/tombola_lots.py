f7d644045fd195860b93ea740124fa1bb6fc41c0
quentin <user.name>
2019-11-09 18:56:41 +0100

    return a json an not a string

--- python/lib/tombola/tombola_lots.py
+++ python/lib/tombola/tombola_lots.py
@@ -70,11 +70,18 @@ class TombolaLots():
                 cur.execute(sql)
                 con.commit()
                 response = cur.fetchall()
+                return_response = {}
+                count = 0
+                for value in response:
+                    return_response[count] = value
+                    count +=1
 
-                return json.dumps(response)
+                return json.loads(json.dumps(return_response))
             except Exception as e:
                 if con:
                     con.rollback()
+
+                print(e)
                 return e
             finally:
                 cur.close()
@@ -119,7 +126,7 @@ class TombolaLots():
             finally:
                 cur.close()
             return self.get_batches()
-            
+
     def update_batch(self, id, name):
         """
         update a batch with its id
